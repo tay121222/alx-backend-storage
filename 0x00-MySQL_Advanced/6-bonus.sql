8d7701a35cdc1e71ac93a5e9b1f1fe9563b8e89a
Enoch Taylor <tay121222@gmail.com>
2024-01-19 12:07:45 +0000

    SQL script that creates a stored procedure AddBonus that adds a new correction for a student

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,7 +3,8 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS AddBonus;
 CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score INT)
 BEGIN
-	INSERT IGNORE INTO projects (name) VALUES (project_name);
+	INSERT INTO projects (name) VALUES (project_name)
+	ON DUPLICATE KEY UPDATE id = LAST_INSERT_ID(id);
 
 	INSERT INTO corrections (`user_id`, `project_id`, score)
 	VALUES (user_id, LAST_INSERT_ID(),score);
