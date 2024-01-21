296eaf00400de5e0e2e87d67bafc4b4686dfa12f
Enoch Taylor <tay121222@gmail.com>
2024-01-21 17:03:09 +0000

    SQL script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- SQL script that creates a view need_meeting
+CREATE VIEW need_meeting AS
+SELECT name FROM students
+WHERE
+    score < 80
+    AND (last_meeting IS NULL OR last_meeting < NOW() - INTERVAL 1 MONTH);
