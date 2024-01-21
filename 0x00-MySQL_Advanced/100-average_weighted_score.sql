54bf4e182bc83e991ad3ab6fa13c1b396e3276e5
Enoch Taylor <tay121222@gmail.com>
2024-01-21 17:26:54 +0000

    SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,19 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_weighted_score FLOAT;
+
+    SELECT AVG(c.score * p.weight)
+    INTO avg_weighted_score
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    WHERE c.user_id = user_id;
+
+    UPDATE users
+    SET average_score = IFNULL(avg_weighted_score, 0)
+    WHERE id = user_id;
+END //
+
+DELIMITER ;
