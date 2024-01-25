a8aff7cf5c407aa4985a3be794383b1afa67a7af
Enoch Taylor <tay121222@gmail.com>
2024-01-25 22:44:14 +0000

    Incrementing values Test Fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,12 +6,25 @@ from typing import Union, Callable, Optional
 from functools import wraps
 
 
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count how many times a method is called."""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
 class Cache:
     def __init__(self):
         """ store an instance of the Redis client as a private variable"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """takes a data argument and returns a string"""
         key = str(uuid.uuid4())
@@ -37,18 +50,6 @@ class Cache:
     def get_int(self, key: str) -> Optional[int]:
         """will automatically parametrize Cache.get"""
         return self.get(key, fn=int)
-
-
-def count_calls(method: Callable) -> Callable:
-    """Decorator to count how many times a method is called."""
-
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        key = method.__qualname__
-        self._redis.incr(key)
-        return method(self, *args, **kwargs)
-
-    return wrapper
 
 
 Cache.store = count_calls(Cache.store)
