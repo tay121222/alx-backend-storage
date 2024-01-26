b000d9ad33e98115e627d8516167c31d04bde0b0
Enoch Taylor <tay121222@gmail.com>
2024-01-26 23:27:19 +0000

    Python function that returns the list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Python function that returns the list of school having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of schoo"""
+    return list(mongo_collection.find({"topics":topic}))
