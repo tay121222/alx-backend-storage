21571ee4147a93d821e703a18834ad29650fc6e1
Enoch Taylor <tay121222@gmail.com>
2024-01-27 04:39:30 +0000

    Improve 12-log_stats.py by adding the top 10 of the most present IPs in the collection nginx of the database logs

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Python script that provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+
+def log_stats():
+    """script that provides some stats"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
+    total_logs = nginx_collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {method: nginx_collection.count_documents(
+        {"method": method}
+        ) for method in methods}
+    status_check_count = nginx_collection.count_documents(
+            {"method": "GET", "path": "/status"}
+            )
+    print("{} logs".format(total_logs))
+    print("Methods:")
+    for method in methods:
+        print("\tmethod {}: {}".format(method, method_counts[method]))
+    print("{} status check".format(status_check_count))
+
+    ips = nginx_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+
+    print("IPs:")
+    for data in ips:
+        print("\t{}: {}".format(data['_id'], data['count']))
+
+
+if __name__ == "__main__":
+    log_stats()
