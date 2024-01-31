de47c8f8e5e20bf7ad1fa51880f33743ca21746b
Enoch Taylor <tay121222@gmail.com>
2024-01-31 05:54:11 +0000

    get_page function cache the result with an expiration time of 10 seconds - edited 3

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,27 +10,23 @@ from typing import Callable
 redis_client = redis.Redis()
 
 
-def track_access_and_cache(expiration: int = 10) -> Callable:
-    """track the number of times a URL is accessed and cache the response."""
+def decorator(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        redis_client.incr("count:{}".format(url))
+        cached_response = redis_client.get("cached:{}".format(url))
+        if cached_response:
+            return cached_response.decode('utf-8')
+        response = func(url)
+        redis_client.setex("cached:{}".format(url), 10, response)
 
-    def decorator(func: Callable) -> Callable:
-        @wraps(func)
-        def wrapper(url: str) -> str:
-            redis_client.incr("count:{}".format(url))
-            cached_response = redis_client.get("cached:{}".format(url))
-            if cached_response:
-                return cached_response.decode('utf-8')
-            response = func(url)
-            redis_client.setex("cached:{}".format(url), expiration, response)
+        return response
 
-            return response
-
-        return wrapper
-
-    return decorator
+    return wrapper
 
 
-@track_access_and_cache()
+@decorator
 def get_page(url: str) -> str:
     """Get the HTML content of given URL"""
-    return requests.get(url).text
+    results = requests.get(url)
+    return results.text
